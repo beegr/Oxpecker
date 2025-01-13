fdeb39067adcdf4006ba1d704626164bdfd53236
Ryan Beegle <rbeegle@mtasolutions.com>
2025-01-12 18:27:55 -0800

    Tests, Examples to F# 8 and net8.0 (mostly)
    Only the Basic example won't go, and that's because of a net9.0 specific feature it uses re: OpenApi

--- examples/WeatherApp/Models.fs
+++ examples/WeatherApp/Models.fs
@@ -5,6 +5,6 @@ open System
 type WeatherForecast = {
     Date: DateOnly
     TemperatureC: int
-    Summary: string | null
+    Summary: string (* null *) 
 } with
     member this.TemperatureF = 32 +  int (float this.TemperatureC / 0.5556)
